95f989d6c4a1742fa496653c0604bec3c16395bb
momo-dev1 <didy1234567@gmail.com>
2024-01-18 17:30:23 +0200

    refactor interface

---
+++ src/interfaces/index.ts
@@ -0,0 +1,42 @@
+export interface IRegisterInput {
+    name: "email" | "password" | "username",
+    type: string,
+    placeholder: string,
+    validation: {
+        required?: boolean,
+        minLength?: number,
+        pattern?: RegExp
+    }
+}
+export interface ILoginInput {
+    name: "identifier" | "password",
+    type: string,
+    placeholder: string,
+    validation: {
+        required?: boolean,
+        minLength?: number,
+        pattern?: RegExp
+    }
+}
+
+export interface IFormRegisterInput {
+    username: string;
+    email: string;
+    password: string;
+}
+
+export interface IFormLoginInput {
+    identifier: string;
+    password: string;
+}
+
+export interface IErrorResponse {
+    error: {
+        // details?: {
+        //   errors: {
+        //     message: string;
+        //   }[];
+        // };
+        message?: string;
+    };
+}

52b38d2a1ec71eb6ef73da8df0ba39dc9315d1db
momo-dev1 <didy1234567@gmail.com>
2024-01-18 12:23:09 +0200

    first commit

---
+++ src/data/index.ts
@@ -0,0 +1,53 @@
+import { IRegisterInput } from "../pages/interfaces"
+
+export const RegisterForm: IRegisterInput[] = [
+    {
+        name: "username",
+        type: "text",
+        placeholder: "Username",
+        validation: {
+            required: true,
+            minLength: 5,
+        }
+    },
+    {
+        name: "email",
+        type: "email",
+        placeholder: "Email address",
+        validation: {
+            required: true,
+            pattern: /^[\w-]+@[a-zA-Z\d-]+\.[a-zA-Z]{2,}$/
+        }
+    },
+
+    {
+        name: "password",
+        type: "password",
+        placeholder: "Password",
+        validation: {
+            required: true,
+            minLength: 6,
+        }
+    },
+]
+
+// export const LOGIN_FORM: ILoginInput[] = [
+//     {
+//       name: "identifier",
+//       placeholder: "Email address",
+//       type: "email",
+//       validation: {
+//         required: true,
+//         pattern: /^[^@ ]+@[^@ ]+\.[^@ .]{2,}$/,
+//       },
+//     },
+//     {
+//       name: "password",
+//       placeholder: "Password",
+//       type: "password",
+//       validation: {
+//         required: true,
+//         minLength: 6,
+//       },
+//     },
+//   ];
---
+++ src/pages/Register.tsx
@@ -0,0 +1,43 @@
+import { useForm, SubmitHandler } from "react-hook-form";
+import Button from "../components/ui/Button";
+import Input from "../components/ui/Input";
+import { IFormInput } from "./interfaces";
+import { RegisterForm } from "../data";
+
+const RegisterPage = () => {
+  const {
+    register,
+    handleSubmit,
+    // formState: { errors },
+  } = useForm<IFormInput>();
+
+  // *------- Handlers -------*
+  const onSubmit: SubmitHandler<IFormInput> = (data) => console.log(data);
+
+  // *------- Renders -------*
+  const renderRegisterForm = RegisterForm.map(
+    ({ type, placeholder, name, validation }, idx) => (
+      <div key={idx}>
+        <Input
+          type={type}
+          placeholder={placeholder}
+          {...register(name, validation)}
+        />
+      </div>
+    )
+  );
+  return (
+    <div className="max-w-md mx-auto">
+      <h2 className="text-center mb-4 text-3xl font-semibold">
+        Register to get access!
+      </h2>
+      <form onSubmit={handleSubmit(onSubmit)} className="space-y-4">
+        {renderRegisterForm}
+
+        <Button fullWidth>Register</Button>
+      </form>
+    </div>
+  );
+};
+
+export default RegisterPage;

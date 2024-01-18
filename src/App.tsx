da1e9a397dd68ffcd71b77085c531bf189e827b0
momo-dev1 <didy1234567@gmail.com>
2024-01-18 17:30:50 +0200

    adding toaster

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { RouterProvider } from "react-router-dom";
+import { Toaster } from "react-hot-toast";
 import router from "./router";
 
 const App = () => {
   return (
     <main>
       <RouterProvider router={router} />
+      <Toaster />
     </main>
   );
 };

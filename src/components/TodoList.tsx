021c64a77b6a74d908a092c97a9337ce63faaa4b
momo-dev1 <didy1234567@gmail.com>
2024-01-26 15:48:59 +0200

    edit todo

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,37 +1,107 @@
+import { useState } from "react";
+import useAuthenticatedQuery from "../hooks/useAuthenticatedQuery";
+import { ITodo } from "../interfaces";
 import Button from "./ui/Button";
+import Input from "./ui/Input";
+import Textarea from "./ui/Textarea";
+import Modal from "./ui/Modal";
 
 const TodoList = () => {
+  const storageKey = "loggedInUser";
+  const userDataString = localStorage.getItem(storageKey);
+  const userData = userDataString ? JSON.parse(userDataString) : null;
+
+  const [isEditModalOpen, setIsEditModalOpen] = useState(false);
+  const [todoToEdit, setTodoToEdit] = useState<ITodo>({
+    id: 0,
+    title: "",
+    description: "",
+  });
+
+  const { data } = useAuthenticatedQuery({
+    queryKey: ["todos"],
+    url: "/users/me?populate=todos",
+    config: {
+      headers: {
+        Authorization: `Bearer ${userData.jwt}`,
+      },
+    },
+  });
+
+  const onCloseEditModal = () => {
+    setTodoToEdit({
+      id: 0,
+      title: "",
+      description: "",
+    });
+    setIsEditModalOpen(false);
+  };
+
+  const onOpenEditModal = (todo: ITodo) => {
+    setTodoToEdit(todo);
+    setIsEditModalOpen(true);
+  };
+
+  const onSubmitUpdateTodo = (e) => {
+    e.preventDefault();
+    setIsEditModalOpen(false);
+  };
+
+  const onChangeHandler = (e) => {};
+
   return (
     <div className="space-y-1 ">
-      <div className="flex items-center justify-between hover:bg-gray-100 duration-300 p-3 rounded-md even:bg-gray-100">
-        <p className="w-full font-semibold">1 - First Todo</p>
-        <div className="flex items-center justify-end w-full space-x-3">
-          <Button size={"sm"}>Edit</Button>
-          <Button variant={"danger"} size={"sm"}>
-            Remove
-          </Button>
-        </div>
-      </div>
+      {data.todos.length ? (
+        data.todos.map((todo: ITodo) => (
+          <div
+            key={todo.id}
+            className="flex items-center justify-between hover:bg-gray-100 duration-300 p-3 rounded-md even:bg-gray-100"
+          >
+            <p className="w-full font-semibold">{todo.title}</p>
+            <div className="flex items-center justify-end w-full space-x-3">
+              <Button size={"sm"} onClick={onOpenEditModal(todo)}>
+                Edit
+              </Button>
+              <Button variant={"danger"} size={"sm"}>
+                Remove
+              </Button>
+            </div>
+          </div>
+        ))
+      ) : (
+        <h3>You have no todos yet!</h3>
+      )}
 
-      <div className="flex items-center justify-between hover:bg-gray-100 duration-300 p-3 rounded-md even:bg-gray-100">
-        <p className="w-full font-semibold">2 - Second Todo</p>
-        <div className="flex items-center justify-end w-full space-x-3">
-          <Button size={"sm"}>Edit</Button>
-          <Button variant={"danger"} size={"sm"}>
-            Remove
-          </Button>
-        </div>
-      </div>
-
-      <div className="flex items-center justify-between hover:bg-gray-100 duration-300 p-3 rounded-md even:bg-gray-100">
-        <p className="w-full font-semibold">3 - Third Todo</p>
-        <div className="flex items-center justify-end w-full space-x-3">
-          <Button size={"sm"}>Edit</Button>
-          <Button variant={"danger"} size={"sm"}>
-            Remove
-          </Button>
-        </div>
-      </div>
+      {/* Edit todo modal */}
+      <Modal
+        isOpen={isEditModalOpen}
+        closeModal={onCloseEditModal}
+        title="Edit this todo"
+      >
+        <form className="space-y-3" onSubmit={onSubmitUpdateTodo}>
+          <Input
+            name="title"
+            value={todoToEdit.title}
+            onChange={onChangeHandler}
+          />
+          <Textarea
+            name="description"
+            value={todoToEdit.description}
+            onChange={onChangeHandler}
+          />
+          <div className="flex items-center space-x-3 mt-4">
+            <Button
+              className="bg-indigo-700 hover:bg-indigo-800"
+              isLoading={isUpdating}
+            >
+              Update
+            </Button>
+            <Button type="button" variant={"cancel"} onClick={onCloseEditModal}>
+              Cancel
+            </Button>
+          </div>
+        </form>
+      </Modal>
     </div>
   );
 };

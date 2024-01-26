3703752cc9f0abec83ee8d156797b6e2587ab10b
momo-dev1 <didy1234567@gmail.com>
2024-01-26 15:37:02 +0200

    adding logout

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,20 +1,43 @@
-import { NavLink } from "react-router-dom";
+import { Link, NavLink, useLocation } from "react-router-dom";
+import Button from "./ui/Button";
 
 const Navbar = () => {
+  const { pathname } = useLocation();
+  const storageKey = "loggedInUser";
+  const userDataString = localStorage.getItem(storageKey);
+  const userData = userDataString ? JSON.parse(userDataString) : null;
+
+  const logOut = () => {
+    localStorage.removeItem("loggedInUser");
+    setTimeout(() => {
+      location.replace(pathname);
+    }, 1500);
+  };
+
   return (
     <nav className="max-w-lg mx-auto mt-7 mb-20 bg-indigo-600 px-3 py-5 rounded-md">
       <ul className="flex items-center justify-between">
         <li className="text-white duration-200 font-semibold text-lg">
           <NavLink to="/">Home</NavLink>
         </li>
-        <p className="flex items-center space-x-3">
-          <li className="text-white duration-200 font-semibold text-lg">
-            <NavLink to="/register">Register</NavLink>
-          </li>
-          <li className="text-white duration-200 font-semibold text-lg">
-            <NavLink to="/login">Login</NavLink>
-          </li>
-        </p>
+
+        {userData ? (
+          <div className="flex items-center space-x-3 text-white text-md">
+            <Link to="/profile">Profile</Link>
+            <Button size={"sm"} onClick={logOut}>
+              LogOut
+            </Button>
+          </div>
+        ) : (
+          <p className="flex items-center space-x-3">
+            <li className="text-white duration-200 font-semibold text-lg">
+              <NavLink to="/register">Register</NavLink>
+            </li>
+            <li className="text-white duration-200 font-semibold text-lg">
+              <NavLink to="/login">Login</NavLink>
+            </li>
+          </p>
+        )}
       </ul>
     </nav>
   );
